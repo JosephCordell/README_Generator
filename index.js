8f772ad024928760a32cbb8f309c6da33ce13310
Joseph Cordell <joseph@jcordell.com>
2021-04-04 11:22:55 -0700

    General Outline

---
+++ index.js
@@ -0,0 +1,36 @@
+/* 
+LICENSE BADGE
+what the app is for, 
+how to use the app, 
+how to install it, 
+how to report issues, 
+license from a list
+and how to make contributions */
+
+
+
+`
+#${data.title}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contribution
+${data.contribution}
+
+## License 
+${data.license}
+
+## Tests
+${data.tests}
+
+## Questions 
+- ${data.GitHub}
+- ${data.email}
+`
